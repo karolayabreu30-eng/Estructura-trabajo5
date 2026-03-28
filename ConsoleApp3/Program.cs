878863d2382da87581f82160a5b0f5eca7ca5dc8
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-27 20:23:11 -0400

    tarea3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio3
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+
+        pila.Push(5);
+        pila.Push(10);
+        pila.Push(15);
+
+        // Eliminar 2 elementos
+        pila.Pop();
+        pila.Pop();
+
+        Console.WriteLine("Estado final de la pila:");
+
+        foreach (int num in pila)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
