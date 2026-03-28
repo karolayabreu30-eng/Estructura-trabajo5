31fa6394c90224cfc91a537da1763ac1869d89a4
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-27 20:24:49 -0400

    tarea4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio4
+{
+    static void Main()
+    {
+        Queue<int> cola = new Queue<int>();
+
+        cola.Enqueue(1);
+        cola.Enqueue(2);
+        cola.Enqueue(3);
+        cola.Enqueue(4);
+        cola.Enqueue(5);
+
+        Console.WriteLine("Contenido de la cola:");
+
+        foreach (int num in cola)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
