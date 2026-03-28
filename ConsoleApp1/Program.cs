21d043c08b923688aed1899cc160bbf069712433
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-27 20:17:28 -0400

    tarea1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio1
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+
+        // Agregar 4 números
+        pila.Push(10);
+        pila.Push(20);
+        pila.Push(30);
+        pila.Push(40);
+
+        // Mostrar contenido
+        Console.WriteLine("Contenido de la pila:");
+        foreach (int num in pila)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
