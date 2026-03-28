9bff0474945e311bd8b2610ad1f95f056a104c0f
karolayabreu30-eng <karolayabreu30@gmail.com>
2026-03-27 20:20:10 -0400

    tarea2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+
+        Console.WriteLine("Eliminando elementos:");
+
+        while (pila.Count > 0)
+        {
+            Console.WriteLine("Eliminado: " + pila.Pop());
+        }
+
+        Console.WriteLine("La pila está vacía.");
+    }
+}
